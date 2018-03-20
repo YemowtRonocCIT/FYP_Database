a4a1406c5eaa62b19c27a61dd16a0ae7c1099d83
Conor Twomey <conor.twomey1@mycit.ie>
2018-03-20 17:41:42 +0000

    Initial commit. Add setup script to repository

---
+++ setup.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE lifebuoy;
+\c lifebuoy
+CREATE TABLE IF NOT EXISTS node(
+    /*
+        sigfox_id is length 16 to allow comfortable extensibility.
+        I cannot find the max length for a sigfox ID.
+    */
+    node_id SERIAL PRIMARY KEY,
+    sigfox_id VARCHAR(16) NOT NULL UNIQUE,
+    active BOOLEAN
+);
+
+INSERT INTO node (node_id, sigfox_id, active)
+VALUES (default, '1D1BF9', true);
+
+INSERT INTO node (node_id, sigfox_id, active)
+VALUES (default, '1D1BFA', false);
+
+CREATE TABLE IF NOT EXISTS messages(
+    message_id SERIAL,
+    node_id INT NOT NULL,
+    button_pressed BOOLEAN,
+    temperature SMALLINT,
+    vibration REAL
+);
+
+CREATE TABLE IF NOT EXISTS sensor(
+    node_id INT NOT NULL,
+    temperature_sensed BOOLEAN,
+    vibration_sensed BOOLEAN
+);
+
